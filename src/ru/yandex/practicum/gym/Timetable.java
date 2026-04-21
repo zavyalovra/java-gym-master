1a9f4f6331c079352e6376cddd22777ac71ede3f
ZavyalovRA <r.zavialov@stcgeo.ru>
2026-04-21 17:49:44 +0500

    Начало работы

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,35 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private HashMap<DayOfWeek, TreeMap<TimeOfDay, ArrayList<TrainingSession>>> timetable;
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
         //сохраняем занятие в расписании
+        DayOfWeek dayOfWeek = trainingSession.getDayOfWeek();
+        TimeOfDay timeOfDay = trainingSession.getTimeOfDay();
+
+        // Проверяем существует ли мапа для указанного дня, если нет, то создаем ее
+        if (!timetable.containsKey(dayOfWeek)) {
+            timetable.put(dayOfWeek, new TreeMap<>());
+        }
+
+        TreeMap<TimeOfDay, ArrayList<TrainingSession>> dayMap = timetable.get(dayOfWeek);
+
+        // Проверяем существует ли список тренировок для указанного дня, если нет, то создаем его
+        if (!dayMap.containsKey(timeOfDay)) {
+            dayMap.put(timeOfDay, new ArrayList<>());
+        }
+
+        dayMap.get(timeOfDay).add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public Map<TimeOfDay, ArrayList<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+
+        return null;
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+    public ArrayList<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        return null;
     }
 }
